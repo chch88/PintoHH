7ad0558277fa1e3e7220fc2bbbc0b0c55843a566
Meddy Richard <richard.meddy57@gmail.com>
2016-10-20 20:37:38 +0200

    fin de recherche bière avec: -recherche -auto complete après recherche

--- page/chercher_biere.php
+++ page/chercher_biere.php
@@ -2,6 +2,13 @@
 
 <?php include("header.php"); ?>
 
+<?php
+// variables contenues dans $_GET
+$nom_biere = isset($_GET['nom_biere']) ? $_GET['nom_biere'] : '';
+$type_biere = isset($_GET['type_biere']) && $_GET['type_biere'] !== '' ? $_GET['type_biere'] : null;
+$provenance_biere = isset($_GET['provenance_biere']) && $_GET['provenance_biere'] !== '' ? $_GET['provenance_biere'] : null;
+
+?>
 
     <div class="container2">
 
@@ -9,15 +16,15 @@
 
         <div class="row">
 
-            <form method="post" action="#">
+            <form method="get" action="">
                 <div class="input-field col l2 s6">
                     <label for="nom">Nom</label>
-                    <input type="text" name="nom" id="nom">
+                    <input type="text" name="nom_biere" id="nom" value="<?= $nom_biere ?>">
                 </div>
 
-
+                <!--
                 <div class="input-field col l2 s6">
-                    <select name="Distance" id="Distance">
+                    <select name="distance_biere" id="Distance">
                         <option>500M</option>
                         <option>1 Km</option>
                         <option>5 Km</option>
@@ -26,38 +33,59 @@
                     <label for="Distance">Palier de distance</label>
                 </div>
 
+
                 <div class="input-field col l2 s6">
-                    <select name="Degre" id="Degre">
+                    <select name="degre_biere" id="Degre">
                         <option>Sans alcool</option>
-                        <option>4 %</option>
-                        <option>7 %</option>
-                        <option>10 %</option>
+                        <option><4 %</option>
+                        <option><7 %</option>
+                        <option><10 %</option>
                     </select>
                     <label for="Degre"> Degré d'alcool </label>
                 </div>
+                -->
 
+                <!-- TYPE BIERE -->
                 <div class="input-field col l2 s6">
-                    <select name="Type" id="Type">
-                        <option>Brune</option>
-                        <option>Blonde</option>
-                        <option>Blanche</option>
-                        <option>Ambrée</option>
-                        <option>Aromatisée</option>
+                    <select name="type_biere" id="Type">
+                        <option value="" disabled selected>Choisissez votre option</option>
+                        <?php
+                        $sql = $bdd->query("SELECT *
+                                FROM type_biere
+                                ");
+
+                        while ($type = $sql->fetch()) { ?>
+                            <option
+                                value="<?= $type['id_type_biere'] ?>" <?= $type_biere == $type['id_type_biere'] ? 'selected' : '' ?>><?= $type['nom_type_biere'] ?></option>
+                            <?php
+                        }
+                        ?>
                     </select>
                     <label for="Type">Type de bière</label>
                 </div>
 
                 <div class="input-field col l2 s6">
-                    <select name="Provenance" id="Provenance">
-                        <option>France</option>
-                        <option>Belgique</option>
-                        <option>Allemagne</option>
+                    <select name="provenance_biere" id="Provenance">
+                        <option value="" disabled selected>Choisissez votre option</option>
+                        <?php
+                        $sql = $bdd->query("SELECT *
+                                FROM pays
+                                ");
+
+                        while ($provenance = $sql->fetch()) { ?>
+                            <option
+                                value="<?= $provenance['id_pays'] ?>" <?= $provenance_biere == $provenance['id_pays'] ? 'selected' : '' ?>><?= $provenance['nom_pays'] ?></option>
+                            <?php
+                        }
+                        ?>
                     </select>
                     <label for="Provenance">Provenance</label>
                 </div>
 
+
+                <!-- RESTRICTIONS
                 <div class="input-field col l2 s6">
-                    <select name="happy" id="Restriction">
+                    <select name="happy_biere" id="Restriction">
                         <option>Pas de restriction</option>
                         <option>Dans la journée</option>
                         <option>Dans une heure</option>
@@ -65,12 +93,27 @@
                     </select>
                     <label for="Restriction">Happy Hour</label>
                 </div>
+                -->
 
                 <div class="input-field col l1 s6 offset-l10 offset-s3">
-                    <a class="waves-effect waves-light btn" type="submit" name="action">Rechercher</a>
+                    <button class="waves-effect waves-light btn" type="submit" name="search">Rechercher
+                    </button>
                 </div>
 
             </form>
+        </div>
+
+        <div class="row">
+            <div class="col l3 offset-l9">
+                <div class="switch center-align">
+                    <label>
+                        Liste
+                        <input type="checkbox">
+                        <span class="lever"></span>
+                        Map
+                    </label>
+                </div>
+            </div>
         </div>
 
         <!-- résultats de la recherche en liste" -->
@@ -79,36 +122,46 @@
                 <ul class="collapsible popout" data-collapsible="accordion">
 
                     <?php
-                    $reponse = $bdd->query
-                    ('SELECT id_biere, nom_type_biere, nom_pays, fichier, nom_biere, degree_biere, prix_normal, prix_happy, description
+                    $query = 'SELECT id_biere, nom_type_biere, nom_pays, fichier, nom_biere, degree_biere, prix_normal, prix_happy, description
                     FROM bieres AS bieres
                     LEFT JOIN type_biere AS type_biere ON bieres.type_biere_id_type_biere = type_biere.id_type_biere
                     LEFT JOIN pays AS pays ON bieres.pays_id_pays = pays.id_pays
                     LEFT JOIN photos AS photos ON bieres.photos_id_photo = photos.id_photo
-                    ');
+                    WHERE nom_biere 
+                    LIKE  "%'.$nom_biere.'%"
+                    ';
+                    if ($type_biere !== null) {
+                        $query = $query . ' AND bieres.type_biere_id_type_biere = ' . $type_biere;
+                    }
+                    if ($provenance_biere !== null) {
+                        $query = $query . ' AND bieres.pays_id_pays = ' . $provenance_biere;
+                    }
+
+                    $reponse = $bdd->query($query);
                     while ($donnees = $reponse->fetch()) {
                         ?>
 
                         <li>
-                            <div class="collapsible-header bar-font"><?php echo $donnees['nom_biere']; ?></div>
+                            <div
+                                class="collapsible-header bar-font center-align"><?php echo $donnees['nom_biere']; ?></div>
                             <div class="collapsible-body white-font">
 
 
                                 <div class="row">
-                                    <div class="col l5">
-                                        <p><?php echo $donnees['degree_biere']?></p>
+                                    <div class="col l6">
+                                        <p><?php echo $donnees['degree_biere'] ?></p>
                                     </div>
 
-                                    <div class="col l3 offset-l4">
+                                    <div class="col l6 right-align">
                                         <p>
-                                            <i class="material-icons prefix">phone</i><?php echo "  " . $donnees['nom_type_biere']; ?>
+                                            Type de bière : <?php echo "  " . $donnees['nom_type_biere']; ?>
                                         </p>
                                     </div>
                                 </div><!-- fin row -->
 
                                 <div class="row">
                                     <div>
-                                        <p>Type de bar : <?php echo $donnees['nom_pays']; ?></p>
+                                        <p>Pays de provenance : <?php echo $donnees['nom_pays']; ?></p>
                                     </div>
                                 </div><!-- fin row -->
 
@@ -116,7 +169,8 @@
                                 <!-- BOUTON POUR LE MODAL -->
                                 <div class="center">
                                     <a class="waves-effect waves-light btn modal-trigger"
-                                       href="#<?php echo $donnees['id_biere']; ?>">Voir la fiche complète de cette bière</a>
+                                       href="#<?php echo $donnees['id_biere']; ?>">Voir la fiche complète de cette
+                                        bière</a>
                                 </div>
 
                                 <br>
@@ -124,7 +178,7 @@
                                 <!-- MODAL - FICHE COMPLETE DU BAR -->
                                 <div id="<?php echo $donnees['id_biere']; ?>" class="modal">
                                     <div class="modal-content grey-font">
-                                        <h4 class="bar-font"><?php echo $donnees['nom_biere']; ?></h4>
+                                        <h4 class="bar-font center-align"><?php echo $donnees['nom_biere']; ?></h4>
                                         <p><?php echo $donnees['description']; ?></p>
                                     </div>
                                     <div class="modal-footer">
@@ -141,15 +195,13 @@
 
                         </li>
 
-
                         <?php
                     }
-
                     ?>
-
                 </ul>
             </div>
         </div>
-    </div>
+
+
 
 <?php include("footer.php"); ?>

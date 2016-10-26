ef66b4101ba1fd06758b01914e42c2d58a55c719
Chudant <ch-ch-@hotmail.fr>
2016-10-26 15:15:51 +0200

    Merge branch 'master' of /var/www/vipere/PintoHH with conflicts.

--- page/chercher_bar.php
+++ page/chercher_bar.php
@@ -80,7 +80,7 @@ $style_bar = isset($_GET['style_bar']) && $_GET['style_bar']!=='' ? $_GET['style
 
     </div> <!-- fin row -->
 
-            <div class="row">
+            <div class="row"> <!-- LISTE OU MAP -->
                 <div class="col l3 offset-l9">
                     <div class="switch">
                         <label>
@@ -101,9 +101,12 @@ $style_bar = isset($_GET['style_bar']) && $_GET['style_bar']!=='' ? $_GET['style
 
                 <?php
                 $sqlQuery = 'SELECT * FROM bars 
-                INNER JOIN styles_bars 
+                LEFT JOIN photos
+                ON bars.photos_id_photo=photos.id_photo
+                LEFT JOIN styles_bars 
                 ON styles_bars.id_style_bar=bars.styles_bars_id_style_bar 
                 WHERE nom_bar LIKE "%'.$nom_bar.'%" 
+                FELFT JOIN 
                 ';
                 if ($style_bar!==null){
                     $sqlQuery = $sqlQuery.' AND bars.styles_bars_id_style_bar = '.$style_bar;
@@ -113,94 +116,141 @@ $style_bar = isset($_GET['style_bar']) && $_GET['style_bar']!=='' ? $_GET['style
                 while ($donnees = $reponse->fetch()) {
                     ?>
 
+                    <!--------------------------------------------- NIVEAU 1 --------------------------------------------->
                     <li>
-                        <div class="collapsible-header bar-font">
+                        <div class="collapsible-header bar-font"> <!-- NIVEAU 1 -->
                             <div class="row row2">
-                                <div class="col l5 s7">
-                                    <?php echo $donnees['nom_bar']; ?>
+                                <div class="col l4 m12 s12">
+                                   <h5> <?php echo $donnees['nom_bar']; ?> </h5>
                                 </div>
 
-                                <div class="col l3 s5">
+                                <div class="col l4 m6 s12">
                                     <i class="material-icons">alarm</i>
-
+                                    Happy Hour en ce moment
                                 </div>
 
-                                <div class="col l3 s5 offset-s7">
+                                <div class="col l3 m5 s10">
                                     <i class="material-icons prefix">location_on</i>
-
+                                    Situé à 1,2 km
                                 </div>
 
-                                <div class="col l1 s5 offset-s7">
-                                    <input type="checkbox" id="test5" />
-                                    <label for="test5"></label>
+                                <div class="col l1 m1 s2">
+                                    <input type="checkbox" id="favoris<?php echo $donnees['id_bar']; ?>" />
+                                    <label for="favoris<?php echo $donnees['id_bar']; ?>"></label>
                                 </div>
+
                             </div> <!-- Fin de la row -->
 
                             <div class="row row2">
-                                <div class="col l3 offset-l5">
+                                <div class="col l4 offset-l4">
                                     <i class="material-icons">euro_symbol</i>
-                                    <h6>Pinte à partir de</h6>
+                                    Pinte à partir de 5€
                                 </div>
                             </div> <!-- Fin de la row -->
 
+                        </div> <!-- FIN NIVEAU 1 -->
 
-
-
-
-
-
-                        </div>
-
-
+                        <!--------------------------------------------- NIVEAU 2 --------------------------------------------->
                         <div class="collapsible-body white-font">
 
-                            <div class="row">
-                                <div class="col l5">
-                                    <p><?php echo $donnees['numero'] . " " . $donnees['rue'] . ", EPINAL" ?></p>
-                                </div>
+                            <div class="row row2">
 
-                                <div class="col l3 offset-l4">
-                                    <p>
+                                    <div class="col l8 m7 s12">
+
+                                        <p>
+                                            <i class="material-icons">gps_fixed</i>
+                                       <?php echo $donnees['numero'] . " " . $donnees['rue'] . ", EPINAL" ?>
+                                        <br>
                                         <i class="material-icons prefix">phone</i><?php echo "  " . $donnees['telephone']; ?>
-                                    </p>
-                                </div>
-                            </div><!-- fin row -->
-
-                            <div class="row">
-                                <div>
-                                    <p>Type de bar : <?php echo $donnees['nom_style_bar']; ?></p>
-                                </div>
-                            </div><!-- fin row -->
+                                            <br>
+                                            <i class="material-icons">blur_on</i>
+                                            Type de bar : <?php echo $donnees['nom_style_bar']; ?>
+                                        </p>
 
 
-                            <!-- BOUTON POUR LE MODAL -->
-                            <div class="center">
-                                <a class="waves-effect waves-light btn modal-trigger"
-                                   href="#<?php echo $donnees['id_bar']; ?>">Voir la fiche complète du bar</a>
-                            </div>
+                                        <!-- BOUTON POUR LE MODAL -->
+                                        <div class="center">
+                                            <a class="waves-effect waves-light btn modal-trigger"
+                                               href="#<?php echo $donnees['id_bar']; ?>">Voir la fiche complète du bar</a>
+                                        </div>
 
-                            <br>
+                                        <br>
 
-                            <!-- MODAL - FICHE COMPLETE DU BAR -->
+                                     </div> <!-- fin partie gauche -->
+
+                                    <div class="col l4 m5 s12">
+                                            <?php
+                                            //afficher une photo
+                                            echo  "<img src='". $donnees['fichier'] . "' width='100%'>";
+                                            ?>
+                                    </div><!-- fin partie droite -->
+
+
+                            </div> <!-- FIN 1ere row -->
+
+
+                            <!--------------------------------------------- NIVEAU 3 --------------------------------------------->
+
+                            <!-- MODAL - FICHE COMPLETE DU BAR - NIVEAU 3-->
                             <div id="<?php echo $donnees['id_bar']; ?>" class="modal">
                                 <div class="modal-content grey-font">
-                                    <h4 class="bar-font"><?php echo $donnees['nom_bar']; ?></h4>
+
+                                    <div class="row">
+                                        <div class="col l4 m12 s12">
+                                            <h5> <?php echo $donnees['nom_bar']; ?> </h5>
+                                        </div>
+
+                                        <div class="col l4 m6 s12">
+                                            <i class="material-icons">alarm</i>
+                                            Happy Hour en ce moment
+                                            <br>
+                                            <i class="material-icons prefix">location_on</i>
+                                            Situé à 1,2 km
+                                            <br>
+                                            <i class="material-icons">euro_symbol</i>
+                                            Pinte à partir de 5€
+                                        </div>
+
+                                        <div class="col l4 m6 s12">
+                                            <i class="material-icons">gps_fixed</i>
+                                            <?php echo $donnees['numero'] . " " . $donnees['rue'] . ", EPINAL" ?>
+                                            <br>
+                                            <i class="material-icons prefix">phone</i><?php echo "  " . $donnees['telephone']; ?>
+                                            <br>
+                                            <i class="material-icons">blur_on</i>
+                                            Type de bar : <?php echo $donnees['nom_style_bar']; ?>
+                                        </div>
+                                    </div> <!-- Fin de la row header-->
+
+
+
+
                                         <p><?php echo utf8_encode($donnees['description']); ?></p>
-                                </div>
+
+
+                                    <div class="carousel">
+                                        <img src="<?php echo ($donnees['fichier']); ?>"></a>
+                                    </div>
+
+
+
+
+
                                 <div class="modal-footer">
                                     <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat"><i
                                             class="large material-icons prefix">close</i></a>
                                 </div>
+                                </div>
 
-                            </div>  <!-- FIN DU MODAL -->
+                            </div>  <!-- FIN DU MODAL NIVEAU 3-->
 
 
-                        </div> <!-- FIN DU COLLAPSE -->
+                        </div> <!-- FIN DU COLLAPSE NIVEAU 2-->
 
 
                     </li>
 
-
+<br>
                     <?php
                 }
 
@@ -208,15 +258,15 @@ $style_bar = isset($_GET['style_bar']) && $_GET['style_bar']!=='' ? $_GET['style
 
             </ul>
 
-                <?php
+              <?php
                 //afficher une photo
-               $sqlQuery = 'SELECT * FROM photos
-               ';
-
-             $reponse = $bdd->query($sqlQuery);
-              while ($donnees = $reponse->fetch()) {
-              echo  "<img src='". $donnees['fichier'] . "' height='100px'>";
-                }
+       //        $sqlQuery = 'SELECT * FROM photos
+         //      ';
+//
+  //           $reponse = $bdd->query($sqlQuery);
+    //          while ($donnees = $reponse->fetch()) {
+      //        echo  "<img src='". $donnees['fichier'] . "' height='100px'>";
+        //        }
 
                 ?>
 
--- scripts.js
+++ scripts.js
@@ -47,3 +47,8 @@ $(document).ready(function() {
 });
 
 
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+        

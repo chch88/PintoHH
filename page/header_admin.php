6e9c5ac55b07a38b690d0290e1199ed9da480b06
Meddy Richard <richard.meddy57@gmail.com>
2016-11-21 10:22:54 +0100

    header admin dylan

---
+++ page/header_admin.php
@@ -0,0 +1,36 @@
+<?php
+// Si tu n'as pas de session active tu peux la démarrer
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <title>PINTO HAPPY HOUR</title>
+
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/PintoHH/materialize/materialize.css">
+    <link rel="stylesheet" type="text/css" href="/PintoHH/feuille.css">
+    <link href="https://fonts.googleapis.com/css?family=Coming+Soon" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="/PintoHH/materialize/materialize.js"></script>
+<script type="text/javascript" src="/PintoHH/scripts.js"></script>
+
+
+
